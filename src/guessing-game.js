3d2a1a721953be6d0acb15ffd1dc6efecfa4a0b9
Yavorskiy Maksim <maxfatal@mail.ru>
2023-02-22 11:24:25 +0300

    solution guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.avg = 0
+    }
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
-
     guess() {
-
+      this.avg =Math.ceil(this.min + (this.max - this.min)/2)
+    //   console.log(`solution candidate is ${this.avg}`);
+      return this.avg
     }
-
     lower() {
-
+        this.max = this.avg
+        // console.log(`min = ${this.min} max = ${this.max}`)
     }
-
     greater() {
-
+        this.min = this.avg
+        // console.log(`min = ${this.min} max = ${this.max}`)
     }
 }
 
